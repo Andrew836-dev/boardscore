834f760f0c6b2ac8f12b1ef8437a1a51b42e1138
Andrew Gray <andrew.aj.gray@gmail.com>
2020-11-16 23:10:04 +1030

    added default values

--- src/utils/playerStore.js
+++ src/utils/playerStore.js
@@ -1,18 +1,18 @@
 import { createContext, useContext, useReducer } from "react";
-import { 
-  addPlayer, 
-  removePlayer, 
-  setLoading, 
-  updateScoreForOnePlayer 
+import {
+  addPlayer,
+  removePlayer,
+  setLoading,
+  updateScoreForOnePlayer
 } from "./reducerFunctions";
-import { 
-  ADD_PLAYER, 
-  LOADING, 
-  REMOVE_PLAYER, 
-  UPDATE_SCORE_FOR_ONE_PLAYER 
+import {
+  ADD_PLAYER,
+  LOADING,
+  REMOVE_PLAYER,
+  UPDATE_SCORE_FOR_ONE_PLAYER
 } from "./actions";
 
-const PlayerContext = createContext();
+const PlayerContext = createContext({ loading: false, players: [] });
 const { Provider } = PlayerContext;
 
 const reducer = (state, action) => {

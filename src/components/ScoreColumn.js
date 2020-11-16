afd114d4f841afdfabf2c955d72f0430d8e25891
Andrew Gray <andrew.aj.gray@gmail.com>
2020-11-17 10:16:09 +1030

    added most of the features for individual player listings

--- src/components/ScoreColumn.js
+++ src/components/ScoreColumn.js
@@ -1,8 +1,32 @@
-function ScoreColumn({ name }) {
+import { useState } from "react";
+import { REMOVE_PLAYER, ADD_A_SCORE } from "../utils/actions";
+
+function ScoreColumn({ dispatch, playerIndex, player }) {
+  const { name, scores } = player || { name: "Something went wrong. Please refresh and try again.", scores: [] };
+  const [scoreEntryVisible, setScoreEntryVisibility] = useState(false);
   return <div>
     <h3>{name}</h3>
-    Score Column
-    </div>;
+    {scoreEntryVisible
+      ? <form
+        onBlur={() => setScoreEntryVisibility(() => false)}
+        onSubmit={event => {
+          event.preventDefault();
+          const formValue = event.currentTarget.elements.score.value;
+          dispatch({ type: ADD_A_SCORE, playerIndex, newScore: formValue });
+          setScoreEntryVisibility(() => false);
+        }}
+      >
+        <input type="number" placeholder="0" name="score" />
+      </form>
+      : <button onClick={() => setScoreEntryVisibility(() => true)}>Add scoring round</button>
+    }
+    {!!scores.length
+      && <ul>
+        {scores.map(scores)}
+      </ul>}
+    Total Score: {!scores.length && '0'}
+    <button onClick={() => dispatch({ type: REMOVE_PLAYER, playerIndex })}>Remove Player</button>
+  </div>;
 }
 
 export default ScoreColumn;
--- src/components/__tests__/ScoreColumn.test.js
+++ src/components/__tests__/ScoreColumn.test.js
@@ -1,22 +1,126 @@
 import React from "react";
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import "@testing-library/jest-dom/extend-expect";
+
+import { REMOVE_PLAYER, ADD_A_SCORE } from "../../utils/actions";
 import ScoreColumn from "../ScoreColumn";
 
 
 test("Loads and displays the passed name", async () => {
-  const testName = "Andrew";
-  render(<ScoreColumn name={testName} />);
+  const testPlayer = { name: "Andrew", scores: [] };
+  render(<ScoreColumn player={testPlayer} />);
 
-  expect(screen.getByText(testName)).toBeInTheDocument();
+  expect(screen.getByText(testPlayer.name)).toBeInTheDocument();
 });
 
 test("Emojis don't cause trouble", async () => {
-  const testName = "🎉😢";
-  render(<ScoreColumn name={testName} />);
+  const testPlayer = { name: "🎉😢", scores: [] };
+  render(<ScoreColumn player={testPlayer} />);
 
-  expect(screen.getByText(testName)).toBeInTheDocument();
+  expect(screen.getByText(testPlayer.name)).toBeInTheDocument();
 });
 
-// test("Has a button to add")
+test("does not throw if given no props", async () => {
+  render(<ScoreColumn />);
+});
+
+test("has a 'remove player' button", async () => {
+  render(<ScoreColumn />);
+
+  expect(screen.getByText(/remove/i)).toBeInTheDocument();
+});
+
+test("When given a playerIndex and dispatch function as props, it implements them with a 'remove player' button", async () => {
+  const testPlayer = { name: "Frank", scores: [] };
+  const testPlayerIndex = 0;
+  const mockDispatch = jest.fn();
+  render(<ScoreColumn player={testPlayer} dispatch={mockDispatch} playerIndex={testPlayerIndex} />);
+
+  act(() => {
+    fireEvent.click(screen.getByText(/remove/i));
+  });
+  expect(mockDispatch).toBeCalledWith({ type: REMOVE_PLAYER, playerIndex: testPlayerIndex });
+});
+
+test("Has a button to add a scoring round", async () => {
+  render(<ScoreColumn />);
+
+  expect(screen.getByText(/add/i)).toBeInTheDocument();
+});
+
+test("Clicking the add score button hides it", async () => {
+  render(<ScoreColumn />);
+
+  const addButton = screen.getByText(/add/i);
+
+  act(() => {
+    fireEvent.click(addButton);
+  });
+
+  expect(addButton).not.toBeInTheDocument();
+});
+
+test("clicking the add score button reveals a form to submit the score", async () => {
+  render(<ScoreColumn />);
+
+  act(() => {
+    fireEvent.click(screen.getByText(/add/i));
+  });
+
+  const scoreForm = screen.getByPlaceholderText(/0/);
+  expect(scoreForm).toBeInTheDocument();
+});
+
+test("losing focus on the form hides it", async () => {
+  render(<ScoreColumn />);
+
+  act(() => {
+    fireEvent.click(screen.getByText(/add/i));
+  });
+
+  const scoreForm = screen.getByPlaceholderText(/0/);
+  expect(scoreForm).toBeInTheDocument();
+
+  act(() => {
+    fireEvent.blur(scoreForm);
+  });
+
+  expect(scoreForm).not.toBeInTheDocument();
+});
+
+test("submitting the form sends a dispatch", async () => {
+  const mockDispatch = jest.fn();
+  const mockPlayerIndex = 2;
+  render(<ScoreColumn dispatch={mockDispatch} playerIndex={mockPlayerIndex} />);
+
+  act(() => {
+    fireEvent.click(screen.getByText(/add/i));
+  });
+
+  const scoreForm = screen.getByPlaceholderText(/0/);
+
+  act(() => {
+    fireEvent.submit(scoreForm);
+  });
+
+  expect(mockDispatch).toBeCalledWith({type: ADD_A_SCORE, playerIndex: mockPlayerIndex, newScore: ""});
+});
+
+test("submitting the form hides the form", async () => {
+  const mockDispatch = () => {};
+  render(<ScoreColumn dispatch={mockDispatch} />)
+
+  const addButton = screen.getByText(/add/i);
+  act(() => {
+    fireEvent.click(addButton);
+  });
+
+  const scoreForm = screen.getByPlaceholderText(/0/);
+  act(() => {
+    fireEvent.submit(scoreForm);
+  });
+
+  expect(scoreForm).not.toBeInTheDocument();
+});
